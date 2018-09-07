6e735af35fd71ace5a3a1740e534d95022c04b66
Eric Fritz <eric@eric-fritz.com>
2018-09-07 16:28:53 -0500

    Update listener to clone and invoke ij.

--- listener/listener.go
+++ listener/listener.go
@@ -1,7 +1,17 @@
 package listener
 
 import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"sync"
+
 	"github.com/efritz/nacelle"
+	"gopkg.in/src-d/go-git.v4"
 
 	"github.com/efritz/ijci/amqp"
 )
@@ -23,11 +33,12 @@ func (l *Listener) Init(config nacelle.Config) error {
 
 func (l *Listener) Start() error {
 	for delivery := range l.Consumer.Deliveries() {
-		l.Logger.Info(
-			"Processing message %d: '%s'\n",
-			delivery.DeliveryTag,
-			string(delivery.Body),
-		)
+		if err := l.handle(string(delivery.Body)); err != nil {
+			l.Logger.Error(
+				"failed to handle message (%s)",
+				err.Error(),
+			)
+		}
 
 		delivery.Ack(false)
 	}
@@ -38,4 +49,67 @@ func (l *Listener) Start() error {
 
 func (l *Listener) Stop() error {
 	return l.Consumer.Shutdown()
+}
+
+func (l *Listener) handle(url string) error {
+	directory, err := ioutil.TempDir("", "build")
+	if err != nil {
+		return err
+	}
+
+	defer os.RemoveAll(directory)
+
+	l.Logger.Info(
+		"Cloning repository %s into %s",
+		url,
+		directory,
+	)
+
+	cloneOptions := &git.CloneOptions{
+		URL:               url,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	}
+
+	if _, err := git.PlainClone(directory, false, cloneOptions); err != nil {
+		return fmt.Errorf(
+			"failed to clone repository (%s)",
+			err.Error(),
+		)
+	}
+
+	command := exec.CommandContext(context.Background(), "/ij", "--no-color")
+	command.Dir = directory
+
+	outReader, err := command.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	errReader, err := command.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() { defer wg.Done(); processOutput(outReader) }()
+	go func() { defer wg.Done(); processOutput(errReader) }()
+
+	if err := command.Run(); err != nil {
+		return err
+	}
+
+	wg.Wait()
+
+	l.Logger.Warning("Build complete")
+	return nil
+}
+
+func processOutput(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		fmt.Printf("> %#v\n", scanner.Text())
+	}
 }
