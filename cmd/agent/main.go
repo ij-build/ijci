9e63aafe4de24be03053af7aad11da8392d57df7
Eric Fritz <eric@eric-fritz.com>
2018-09-09 20:27:38 -0500

    Remove grpc setup from agent.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"github.com/efritz/nacelle"
-	"github.com/efritz/scarf"
-	"github.com/efritz/scarf/logging"
 
 	"github.com/efritz/ijci/amqp"
-	"github.com/efritz/ijci/grpc"
 	"github.com/efritz/ijci/handler"
 	"github.com/efritz/ijci/listener"
 )
@@ -25,18 +22,6 @@ func setup(processes nacelle.ProcessContainer, services nacelle.ServiceContainer
 	processes.RegisterProcess(
 		listener.NewListener(),
 		nacelle.WithProcessName("listener"),
-	)
-
-	// TODO - make a scarf function for this
-
-	processes.RegisterInitializer(
-		logging.NewInitializer(scarf.DefaultExtractors),
-		nacelle.WithInitializerName("log decorator"),
-	)
-
-	processes.RegisterProcess(
-		scarf.NewServer(grpc.NewEndpointSet()),
-		nacelle.WithProcessName("server"),
 	)
 
 	return nil
